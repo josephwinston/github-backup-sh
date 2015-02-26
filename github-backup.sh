2ac42d64a92038188c1919216d9fa60c585f0f75
Jody Winston <joseph.winston@halliburton.com>
2015-02-26 08:14:43 -0600

    Use page count

--- github-backup.sh
+++ github-backup.sh
@@ -14,10 +14,23 @@ DATE=$(date +"%Y%m%d")
 TEMP_DIR="github_${USER}_${DATE}"
 BACKUP_FILE="${TEMP_DIR}.tgz"
 
-mkdir "$TEMP_DIR" && cd "$TEMP_DIR"
-curl -s "$API_URL" | grep -Eo '"git_url": "[^"]+"' | awk '{print $2}' | xargs -n 1 git clone
+#
+# Find out how many pages we have
+#
+
+PAGES=`curl -I "${API_URL}"|grep "Link"|cut -f4 -d' '|cut -f2 -d'&'|cut -f2 -d'='|cut -f1 -d'>'`
+
+mkdir "${TEMP_DIR}" && cd "${TEMP_DIR}"
+
+count=1
+while [ $count -le ${PAGES} ]
+do
+    curl -s "${API_URL}?page=${count}" | grep -Eo '"git_url": "[^"]+"' | awk '{print $2}' | xargs -n 1 git clone
+    count=`expr ${count} + 1`
+done
+
 cd -
-tar zcf "$BACKUP_FILE" "$TEMP_DIR"
-rm -rf "$TEMP_DIR"
+# tar zcf "$BACKUP_FILE" "$TEMP_DIR"
+# rm -rf "$TEMP_DIR"
 
 
